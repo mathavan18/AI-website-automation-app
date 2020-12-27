cc02b72b8ca6bbd58de500af0e6230fdb85ef904
DESKTOP-3GKT9EU\matha <mathavan18@gmail.com>
2020-12-27 16:58:12 -0500

    Added backend URL

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmdp4lz_1Up_QxORtwfVDhIlXoQBPayOA",
+  authDomain: "ai-website-automation-fde83.firebaseapp.com",
+  projectId: "ai-website-automation-fde83",
+  storageBucket: "ai-website-automation-fde83.appspot.com",
+  messagingSenderId: "436853082483",
+  appId: "1:436853082483:web:8ec5097dbff9ad8de55d06",
+  measurementId: "G-D39XFM34D0",
+};
