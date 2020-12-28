edd5a91e096b61211f8405c8489864b9ea6784df
DESKTOP-3GKT9EU\matha <mathavan18@gmail.com>
2020-12-28 07:38:19 -0500

    Removed bootstrap module

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://ai-website-automation.herokuapp.com/",
-  //baseURL: "http://localhost:8001/",
 });
 
 export default instance;
